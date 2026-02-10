806a1292c54df9837d4d26187703ecf49a43e828
Jinesh-solulab <jinesh@solulab.co>
2026-02-10 15:34:55 +0530

    Add basic SEO meta tags

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Helmet } from "react-helmet-async";
 
 function FAQ() {
   return (
+
+    <Helmet>
+
+        <title>Website</title>
+
+        <meta name="description" content="Website description." />
+
+        <meta name="robots" content="index,follow" />
+
+        <meta property="og:title" content="Website" />
+
+        <meta property="og:description" content="Website description." />
+
+        <meta name="twitter:card" content="summary" />
+
+        <meta name="twitter:title" content="Website" />
+
+        <meta name="twitter:description" content="Website description." />
+
+      </Helmet>
+
 
 
 
